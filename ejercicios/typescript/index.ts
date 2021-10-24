82fe8922eba8ccccb94b40cebaaa4d4b5f9a290d
Marcelino2017 <marcelinomebe@gmail.com>
2021-10-24 11:45:24 -0500

    Funciones TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -28,15 +28,37 @@ enum Color {
     Verde = 'Verde',
     Azul = 'Azul',
     Amarillo = 'Amarillo',
-  }
+}
   
-  let colorFavorito: Color = Color.Amarillo;
-  console.log(`Mi color favorito es ${colorFavorito}`);
+let colorFavorito: Color = Color.Amarillo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = 'Joker';
+comodin = { type: 'Wildcard' };
+
+// Object
+let someObject: object = { type: 'Wildcard' };
+
+// Funciones
+function add(a: number, b: number): number {
+    return a + b;
+}
   
-  // Any
-  let comodin: any = 'Joker';
-  comodin = { type: 'Wildcard' };
-  
-  // Object
-  let someObject: object = { type: 'Wildcard' };
-  
+const sum = add(4, 6);
+
+function createAdder(a: number): (number) => number {
+    return function(b: number) {
+        return b + a;
+    };
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+return `${firstName} ${lastName}`;
+}
+
+const richard = fullName('Agente');
+console.log(richard);
